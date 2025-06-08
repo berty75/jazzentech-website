872c44343f8ae11db04a25b0ac248d70a52cf556
Bertrand GENDRIN <b.gendrin76@gmail.com>
2025-06-08 18:50:04 +0200

    Add redirections for jazzentech.fr domain

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,40 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'jazzentech.fr',
+          },
+        ],
+        destination: 'https://www.jazzentech.com/:path*',
+        permanent: true,
+      },
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'www.jazzentech.fr',
+          },
+        ],
+        destination: 'https://www.jazzentech.com/:path*',
+        permanent: true,
+      },
+    ]
+  },
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+module.exports = nextConfig
